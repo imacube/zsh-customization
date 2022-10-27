5913fc2b5af0ed5c7792337f621d4870f4ea5654
Ryan Steele <ryan@imacube.net>
2022-10-27 14:10:12 -0700

    Adding personal alias entries

---
+++ personal.zsh
@@ -0,0 +1,8 @@
+##
+# Personal ZSH customizations
+##
+
+alias less='less --use-color'
+alias gl='git log --graph --decorate --all'
+alias glo='git log --graph --decorate --all --oneline'
+alias glp='git log --graph --decorate --all -p'
